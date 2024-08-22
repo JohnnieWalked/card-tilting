ee1076ad3de8123c58e1c093122246230d74ac84
iddqd <mgcmasterchief@gmail.com>
2024-08-22 21:31:45 +0300

    feat: added crucial functionality (tilting, shadows, multiple layers);

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+/* hooks */
+import { useThrottle } from './hooks/useThrottle';
+
+/* imgs */
+import firstImgLayer from './assets/3d-marshall (first layer).png';
+import secondImgLayer from './assets/3d-marshall (second layer).png';
+import thirdImgLayer from './assets/3d-marshall (third layer).png';
+
+/* styles */
+import './App.css';
+
+export default function App() {
+  const [elementPos, setElementPos] = useState({ x: 0, y: 0 });
+  const throttledValue = useThrottle(elementPos, 100);
+
+  function rotateElement(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const card = event.currentTarget.getBoundingClientRect();
+
+    /* mouse pos inside card */
+    const x = event.clientX - card.left;
+    const y = event.clientY - card.top;
+
+    /* find middle of card */
+    const middleX = card.width / 2;
+    const middleY = card.height / 2;
+
+    /* get offset from middle */
+    const offsetX = ((x - middleX) / middleX) * 25;
+    const offsetY = ((y - middleY) / middleY) * 25;
+    setElementPos({ x: offsetX, y: offsetY });
+  }
+
+  function onMouseLeave() {
+    setElementPos({ x: 0, y: 0 });
+  }
+
+  useEffect(() => {
+    console.log(throttledValue.x, throttledValue.y);
+  }, [throttledValue]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section>
+      <div
+        onMouseMove={(e) => rotateElement(e)}
+        onMouseLeave={onMouseLeave}
+        className="card-tilting"
+        style={{
+          transform: `perspective(5000px) rotateX(${
+            -1 * throttledValue.y
+          }deg) rotateY(${throttledValue.x}deg)`,
+        }}
+      >
+        <div className="layer first-layer">
+          <img src={firstImgLayer} alt="marshall-first-img-layer" />
+        </div>
+        <div className="layer second-layer">
+          <img src={secondImgLayer} alt="marshall-second-img-layer" />
+        </div>
+        <div className="layer third-layer">
+          <img src={thirdImgLayer} alt="marshall-third-img-layer" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
-
-export default App
